5050cb55f63321d6d2defad0a9c55e0ed741d27d
jorge pallol <jorgepallol1@gmail.com>
2024-09-15 17:34:25 +0200

    feature : configuration a Token for change the password (Recoord)

--- src/main/java/com/shoppingcart/user/service/password/PasswordRecoverService.java
+++ src/main/java/com/shoppingcart/user/service/password/PasswordRecoverService.java
@@ -8,11 +8,10 @@ import com.shoppingcart.user.service.notification.UserNotificationService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Slf4j
 @Service
@@ -32,18 +31,39 @@ public class PasswordRecoverService {
 
         final UserEntity userEntity = optionalUser.get();
 
-        return userNotificationService.sendRecoveryPasswordNotification(UserDto
+        final UserDto user = UserDto
                 .builder()
                 .email(userEntity.getEmail())
                 .name(userEntity.getName())
                 .address(userEntity.getAddress())
                 .phoneNumber(userEntity.getPhoneNumber())
                 .createdAt(userEntity.getCreatedAt())
-                .build());
+                .tokenPasswordCreatedAt(new Date())
+                .token(UUID.randomUUID().toString())
+                .build();
+
+        tokenUserMap.put(user.getToken(),user);
+
+        return userNotificationService.sendRecoveryPasswordNotification(user);
     }
 
+    // Que es un CRON: es una tarea programada con periodos de ejecucion
+    // * en cualquier momento podemos trabajar numeros 0 - 60 segundos, minutos, 0 - 23 horas, 0 - 6 dias de semana, dias del mes 30,29,28 etc
+    // 0/1 -> ejecutarse al momento cero cada minuto , */1 -> se va a ejecutar al momento cualquiera cada minuto
+    // Segundos    ->  0
+    // Minutos     -> */1
+    // Horas       ->  *
+    // Dias        ->  ? cualquier se usa para dias y semanas
+    // Semanas     -> * cualquier
+    // Meses       -> * cualquier mes
+
+    @Scheduled(cron = "0 */1 * ? * *")
     public void verifyTokenExpiryEveryMinute(){
 
+        tokenUserMap.forEach((token,user)->{
+            //TODO comprobar que si ha pasado mas de 10 minutos borrar el token del mapa
+
+        });
     }
 
     public void changePassword(String email, String token,String password,String password2){
